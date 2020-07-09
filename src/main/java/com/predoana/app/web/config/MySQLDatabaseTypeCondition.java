7a233dda6458ed180161f1eff8ad82ca9112d6b3
ClaudiuPredoana <claudiupredoana@gmail.com>
2020-07-09 20:15:12 +0300

    implement the condition to check whether the dbType system property is MYSQL

--- src/main/java/com/predoana/app/web/config/MySQLDatabaseTypeCondition.java
+++ src/main/java/com/predoana/app/web/config/MySQLDatabaseTypeCondition.java
@@ -1,4 +1,4 @@
-package com.predoana.app.web;
+package com.predoana.app.web.config;
 
 import org.springframework.context.annotation.Condition;
 import org.springframework.context.annotation.ConditionContext;
