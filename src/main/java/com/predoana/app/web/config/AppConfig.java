463ea05c20030d7836fafaf990501ce202e8518b
ClaudiuPredoana <claudiupredoana@gmail.com>
2020-07-10 12:07:00 +0300

    bind property values into bean properties using @Value

--- src/main/java/com/predoana/app/web/config/AppConfig.java
+++ src/main/java/com/predoana/app/web/config/AppConfig.java
@@ -1,18 +1,15 @@
 package com.predoana.app.web.config;
 
-import com.predoana.app.web.dao.JdbcUserDAO;
-import com.predoana.app.web.dao.UserDAO;
+import org.springframework.beans.factory.annotation.Value;
 
-import org.springframework.context.annotation.ComponentScan;
-import org.springframework.context.annotation.Configuration;
-
-@Configuration
-@ComponentScan
 public class AppConfig {
-
-    @DatabaseType("MYSQL")
-    public UserDAO jdbcUserDAO() {
-        return (UserDAO) new JdbcUserDAO();
-    }
+    @Value("${jdbc.driver}")
+    private String driver;
+    @Value("${jdbc.url}")
+    String url;
+    @Value("${jdbc.username}")
+    private String username;
+    @Value("${jdbc.password}")
+    private String password;
 
 }
