75fb1611f3a85a7f4f53e25565d3d5cb374cdb12
ClaudiuPredoana <claudiupredoana@gmail.com>
2020-07-11 12:48:49 +0300

    create a database setup script

---
+++ src/main/resources/data.sql
@@ -0,0 +1,9 @@
+DROP TABLE IF EXISTS USERS;
+CREATE TABLE users (
+id int(11) NOT NULL AUTO_INCREMENT,
+ name varchar(100) NOT NULL,
+ email varchar(100) DEFAULT NULL,
+ PRIMARY KEY (id) );
+insert into users(id, name, email) values(1,'John','john@gmail.com');
+ insert into users(id, name, email) values(2,'Rod','rod@gmail.com');
+ insert into users(id, name, email) values(3,'Mike','mike@gmail.com');
