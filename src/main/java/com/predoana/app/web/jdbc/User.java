52e16dde78e58ded6e23b8f7ffad4360caa28f62
ClaudiuPredoana <claudiupredoana@gmail.com>
2020-07-11 12:51:48 +0300

    inject JdbcTemplate into the data access components to interact with the databases

---
+++ src/main/java/com/predoana/app/web/jdbc/User.java
@@ -0,0 +1,31 @@
+package com.predoana.app.web.jdbc;
+
+public class User {
+    public Integer id;
+    public String name;
+    public String email;
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+}
