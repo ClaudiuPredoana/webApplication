784379edaa18a64282e8cc31e36cdb133fc6232e
ClaudiuPredoana <claudiupredoana@gmail.com>
2020-07-14 12:42:08 +0300

    Adding the spring-boot-starter-jdbc module

--- src/main/java/com/predoana/app/web/jdbc/UserRowMapper.java
+++ src/main/java/com/predoana/app/web/jdbc/UserRowMapper.java
@@ -4,7 +4,7 @@ import javax.swing.tree.RowMapper;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 
-public class UserRowMapper implements RowMapper {
+public class UserRowMapper implements RowMapper<User> {
     @Override
     public User mapRow(ResultSet rs, int rowNum) throws SQLException {
         User user = new User();
