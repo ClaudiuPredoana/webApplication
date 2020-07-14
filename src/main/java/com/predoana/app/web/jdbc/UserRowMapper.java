25153e6ae628c413394ec3f5337dc54aee4395a2
ClaudiuPredoana <claudiupredoana@gmail.com>
2020-07-14 13:43:18 +0300

    Inject JdbcTemplate into UserRepository

--- src/main/java/com/predoana/app/web/jdbc/UserRowMapper.java
+++ src/main/java/com/predoana/app/web/jdbc/UserRowMapper.java
@@ -1,17 +1,15 @@
-package com.predoana.app.web.jdbc;
+import com.predoana.app.web.jdbc.User;
 
 import javax.swing.tree.RowMapper;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 
-public class UserRowMapper implements RowMapper<User> {
+class UserRowMapper implements RowMapper<User> {
+
     @Override
     public User mapRow(ResultSet rs, int rowNum) throws SQLException {
         User user = new User();
-    }        user.setId(rs.getInt("id"));
- user.setName(rs.getString("name"));
- user.setEmail(rs.getString("email"));
-        return user;
-} }
-
-
+        user.setId(rs.getInt("id"));
+        user.setName(rs.getString("name"));
+        user.setEmail(rs.getString("email"));
+        return user;    } }
