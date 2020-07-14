faed9179c24d38e57bb19ea5fbc96e4f69272a24
ClaudiuPredoana <claudiupredoana@gmail.com>
2020-07-14 13:36:14 +0300

    Inject JdbcTemplate into UserRepository

--- src/main/java/com/predoana/app/web/jdbc/UserRepository.java
+++ src/main/java/com/predoana/app/web/jdbc/UserRepository.java
@@ -4,6 +4,8 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.autoconfigure.jdbc.JdbcTemplateAutoConfiguration;
 import org.springframework.stereotype.Repository;
 
+import java.sql.Connection;
+import java.sql.PreparedStatement;
 import java.util.List;
 
 @Repository
@@ -11,8 +13,24 @@ public class UserRepository {
     @Autowired
     private JdbcTemplateAutoConfiguration jdbcTemplate;
 
-    @Transactional(readOnly=true)
+ @Transactional(readOnly=true)
     public List<User> findAll() {
         return jdbcTemplate.query("select * from users", new UserRowMapper());
-    } }
+    }
+
+    @Transactional(readOnly=true)
+    public User findUserById(int id) {
+    return jdbcTemplate.queryForObject("select * from users where id=?",                                        new Object[]{id}, new UserRowMapper());
+}
+    public User create(final User user) {
+     final String sql = "insert into users(name,email) values(?,?)";
+        KeyHolder holder = new GeneratedKeyHolder();
+        jdbcTemplate.update(new PreparedStatementCreator() {
+
+            @Override
+            public PreparedStatement createPreparedStatement(Connection connection)                        throws SQLException {                    PreparedStatement ps = connection                            .prepareStatement(sql, Statement.RETURN_GENERATED_KEYS);                    ps.setString(1, user.getName());                    ps.setString(2, user.getEmail());                    return ps;                }            }, holder);
+        int newUserId = holder.getKey().intValue();
+        user.setId(newUserId);
+        return user;
+ } }
 
